a266c918e8c651d3e6385ce44380f79af6b68b0d
Xavier Laptop <xgagnon1@gmail.com>
2023-06-13 09:51:05 -0600

    Create UserService class and UserServiceTest class

---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,30 @@
+package services;
+
+import dbaccess.UserDB;
+import models.User;
+
+import java.util.List;
+
+public class UserService {
+    UserDB userDb = UserDB.getInstance();
+
+    public void insert(User user) {
+
+    }
+
+    public void insertMany(List<User> users) {
+
+    }
+
+    public void find(int userId) {
+
+    }
+
+    public void update(User user) {
+
+    }
+
+    public void delete(User user) {
+
+    }
+}
---
+++ src/test/java/services/UserServiceTest.java
@@ -0,0 +1,28 @@
+package services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void insertMany() {
+    }
+
+    @Test
+    public void find() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
