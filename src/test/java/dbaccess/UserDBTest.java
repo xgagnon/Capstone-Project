8ed02d716790bccfc36327a9abc9c03bb41f2d4e
Xavier Laptop <xgagnon1@gmail.com>
2023-06-13 08:12:11 -0600

    Complete delete test under UserDB

--- src/test/java/dbaccess/UserDBTest.java
+++ src/test/java/dbaccess/UserDBTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class UserDBTest {
 
@@ -105,6 +105,40 @@ public class UserDBTest {
 
     @Test
     public void testDelete() {
+        int userId = 10001007;
+        String email = "user6@example.com";
+        String firstName = "Jose";
+        String lastName = "Loewen";
+        long phone = 4039876543L;
+        String address = "163 Bass St, City";
+        String password = "hashed_password";
+        Role role = Role.user;
+
+        User user = new User();
+        user.setUserId(userId);
+        user.setEmail(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPhone(phone);
+        user.setAddress(address);
+        user.setPassword(password);
+        user.setRole(role);
+
+        user.getCart().add(123456789012L);
+        user.getCart().add(678901234567L);
+        user.getLikes().add(345678901234L);
+        user.getLikes().add(234567890123L);
+        user.getLikes().add(987654321098L);
+        user.getTransactions().add(123456789003L);
+
+        userDb.insert(user);
+        User foundUser = userDb.find(user.getUserId());
+        assertNotNull(foundUser);
+
+        userDb.delete(foundUser);
+        foundUser = userDb.find(user.getUserId());
+        assertNull(foundUser);
+
     }
 
 }
