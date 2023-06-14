ff54fb27668250b3ef92fb2aaaa5abb075e6eda7
Ravneet Gill <ravneetjargari@gmail.com>
2023-06-14 08:33:43 -0600

    Image and Transaction Class done

--- src/main/java/models/Image.java
+++ src/main/java/models/Image.java
@@ -1,4 +1,67 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Image {
+
+    private int imageId;
+
+    private String title;
+
+    private String description;
+
+    private String seller;
+
+    private int likes;
+
+    private int views;
+
+    private int price;
+
+    private String status;
+
+    private String imageLocation;
+
+    private List<Long> tags = new ArrayList<>();
+
+    public int getImageId(){return imageId;}
+
+    public void setImageId(int imageId){this.imageId=imageId;}
+
+    public String getTitle() {return title;}
+
+    public void setTitle(String title) {this.title=title;}
+
+    public String getDescription(){return description;}
+
+    public void setDescription(String description){ this.description=description;}
+
+    public String getSeller(){return seller;}
+
+    public void setSeller(String seller){this.seller=seller;}
+
+    public int getLikes(){return likes;}
+
+    public void setLikes(int likes){this.likes=likes;}
+
+    public int getViews(){return views;}
+
+    public void setViews(int views){this.views=views;}
+
+    public int getPrice(){return price;}
+
+    public void setPrice(int price){this.price=price;}
+
+    public String getStatus(){return status;}
+
+    public void setStatus(String Status){ this.status=status;}
+
+    public String getImageLocation(){return imageLocation;}
+
+    public void setImageLocation(String imageLocation){this.imageLocation=imageLocation;}
+
+    public List<Long> getTags() {return tags;}
+
+    public void setTags(List<Long> tags) {this.tags = tags;}
 }
--- src/main/java/models/Transaction.java
+++ src/main/java/models/Transaction.java
@@ -1,4 +1,42 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transaction {
+
+    private int transactionId;
+    private String date;
+
+    private double price;
+
+    private String buyerEmail;
+
+    private  String status;
+
+    private List<Long> purchasedImages = new ArrayList<>();
+
+    List <Long> images= new ArrayList<>();
+
+    public int getTransactionId() {return transactionId;}
+
+    public void setTransactionId(int transactionId) {this.transactionId = transactionId;}
+
+    public String getDate() {return date;}
+
+    public void setDate(String date) {this.date = date;}
+
+    public String getBuyerEmail() {return buyerEmail;}
+
+    public void setBuyerEmail(String buyerEmail) {this.buyerEmail = buyerEmail;}
+
+    public String getStatus() {return status;}
+
+    public void setStatus(String status) {this.status = status;}
+
+    public double getPrice() {return price;}
+
+    public void setPrice(double price) {this.price = price;}
+
+    public List<Long> getPurcahsedImages() {return purchasedImages;}
 }
