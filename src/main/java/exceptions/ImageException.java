3a3be6739266fc53805f836cf88d8ac469a5ebff
Xavier Laptop <xgagnon1@gmail.com>
2023-06-20 10:28:38 -0600

    Add code to exception classes

--- src/main/java/exceptions/ImageException.java
+++ src/main/java/exceptions/ImageException.java
@@ -1,7 +1,13 @@
 package exceptions;
 
 public class ImageException  extends Exception{
+
+    private final String code = "E1003";
     public ImageException(String errorMessage) {
         super(errorMessage);
+    }
+
+    public String getCode() {
+        return code;
     }
 }
--- src/main/java/exceptions/TransactionException.java
+++ src/main/java/exceptions/TransactionException.java
@@ -1,7 +1,13 @@
 package exceptions;
 
 public class TransactionException  extends Exception{
+
+    private final String code = "E1002";
     public TransactionException(String errorMessage) {
         super(errorMessage);
+    }
+
+    public String getCode() {
+        return code;
     }
 }
