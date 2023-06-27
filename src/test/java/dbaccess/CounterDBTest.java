9ce7ff49c5ae9f7347da7f98d605e03804a94fa3
Xavier Laptop <xgagnon1@gmail.com>
2023-06-27 09:18:18 -0600

    Update counter classes

--- src/test/java/dbaccess/CounterDBTest.java
+++ src/test/java/dbaccess/CounterDBTest.java
@@ -1,22 +1,40 @@
 package dbaccess;
 
 import models.Counter;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CounterDBTest {
 
     CounterDB counterDB = CounterDB.getInstance();
+
     @Test
-    public void testFind() {
+    public void testFindUserId() {
         Counter counter = counterDB.find("userid");
         assertNotNull(counter);
         assertEquals(10007, counter.getSeq());
     }
 
     @Test
-    public void testUpdate() {
+    public void testFindImageId() {
+        Counter counter = counterDB.find("imageId");
+        assertNotNull(counter);
+        assertEquals(1000000010, counter.getSeq());
+    }
+
+    @Test
+    public void testFindTransactionId() {
+        Counter counter = counterDB.find("transactionId");
+        assertNotNull(counter);
+        assertEquals(1000000006, counter.getSeq());
+    }
+
+    @Test
+    public void testUpdateUserId() {
         Counter counter = counterDB.find("userid");
         assertNotNull(counter);
         assertEquals(10007, counter.getSeq());
@@ -27,6 +45,40 @@ public class CounterDBTest {
         Counter updatedCounter = counterDB.find("userid");
         assertNotNull(counter);
         assertEquals(10008, updatedCounter.getSeq());
+
+        //After Update test, make sure to manually change counter
+        //back to original value (10007)
+    }
+
+    @Test
+    public void testUpdateImageId() {
+        Counter counter = counterDB.find("imageId");
+        assertNotNull(counter);
+        assertEquals(1000000010, counter.getSeq());
+
+        counter.incrementSeq();
+        counterDB.update(counter);
+
+        Counter updatedCounter = counterDB.find("imageId");
+        assertNotNull(counter);
+        assertEquals(1000000011, updatedCounter.getSeq());
+
+        //After Update test, make sure to manually change counter
+        //back to original value (10007)
+    }
+
+    @Test
+    public void testUpdateTransactionId() {
+        Counter counter = counterDB.find("transactionId");
+        assertNotNull(counter);
+        assertEquals(1000000006, counter.getSeq());
+
+        counter.incrementSeq();
+        counterDB.update(counter);
+
+        Counter updatedCounter = counterDB.find("transactionId");
+        assertNotNull(counter);
+        assertEquals(1000000007, updatedCounter.getSeq());
 
         //After Update test, make sure to manually change counter
         //back to original value (10007)
