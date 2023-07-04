4aa759d4f8eeb816349349e759f110c6698e3a46
David <david.doan01@edu.sait.ca>
2023-07-04 14:00:50 -0600

    Created transaction validation testing file.

---
+++ src/test/java/validation/transactionValidationTest.java
@@ -0,0 +1,14 @@
+package validation;
+
+
+import javax.validation.constraints.*;
+import java.util.List;
+
+public class transactionValidationTest {
+
+    TransactionDB transactionDb = TransactionDB.getInstance();
+
+    @Test
+    public void test
+}
+
