5bb8337d42d5940051ddd4d3928dcc48421b64b3
Xavier Laptop <xgagnon1@gmail.com>
2023-07-04 09:48:59 -0600

    Commit build.gradle

--- src/main/java/dbaccess/ImageDB.java
+++ src/main/java/dbaccess/ImageDB.java
@@ -1,7 +1,6 @@
 package dbaccess;
 
 import com.mongodb.MongoException;
-<<<<<<< HEAD
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -10,12 +9,6 @@ import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.Updates;
-=======
-
-import com.mongodb.client.*;
-
-import com.mongodb.client.model.*;
->>>>>>> ravneet
 import models.Image;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecProvider;
@@ -32,17 +25,14 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class ImageDB {
-<<<<<<< HEAD
 
-=======
->>>>>>> ravneet
     public static final String URI = "mongodb://localhost:27017";
     public static final String DB_NAME = "image_store_db";
     public static final String COLLECTION_NAME = "images";
 
     public static final String imageIdField = "imageId";
     public static final String titleField = "title";
-<<<<<<< HEAD
+
     public static final String descriptionField  = "description";
     public static final String sellerField = "seller";
     public static final String likesField =  "likes";
@@ -50,16 +40,16 @@ public class ImageDB {
     public  static final String priceField = "price";
     public static final String statusField = "status";
     public static final String imageLocationField = "imageLocation";
-=======
-    public static final String descriptionField = "description";
-    public static final String sellerField = "seller";
-    public static final String likesField = "likes";
-    public static final String viewsField = "views";
-    public static final String priceField = "price";
-    public static final String statusField = "status";
-    public static final String imageLocationField = "imageLocation";
 
->>>>>>> ravneet
+//    public static final String descriptionField = "description";
+//    public static final String sellerField = "seller";
+//    public static final String likesField = "likes";
+//    public static final String viewsField = "views";
+//    public static final String priceField = "price";
+//    public static final String statusField = "status";
+//    public static final String imageLocationField = "imageLocation";
+
+
     public static final String tagsField = "tags";
 
     private static ImageDB instance;
@@ -79,10 +69,6 @@ public class ImageDB {
 
             MongoDatabase database = mongoClient.getDatabase(DB_NAME);
             MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
-<<<<<<< HEAD
-
-=======
->>>>>>> ravneet
             try {
                 collection.insertOne(new Document()
                         .append("_id", new ObjectId())
@@ -95,11 +81,8 @@ public class ImageDB {
                         .append(priceField, image.getPrice())
                         .append(statusField, image.getStatus())
                         .append(imageLocationField, image.getImageLocation())
-<<<<<<< HEAD
                         .append(tagsField,image.getTags())
-=======
                         .append(tagsField, image.getTags())
->>>>>>> ravneet
                 );
             } catch (MongoException me) {
                 System.err.println("Unable to insert due to an error: " + me);
@@ -107,27 +90,15 @@ public class ImageDB {
         }
     }
 
-<<<<<<< HEAD
-
     public void insertMany(List<Image> images) {
-
-=======
-    public void insertMany(List<Image> images) {
->>>>>>> ravneet
         try (MongoClient mongoClient = MongoClients.create(URI)) {
 
             MongoDatabase database = mongoClient.getDatabase(DB_NAME);
             MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
 
             List<Document> imageList = new ArrayList<>();
-<<<<<<< HEAD
-
-            for(Image image : images) {
-                imageList.add(new Document()
-=======
             for (Image image : images) {
                 collection.insertOne(new Document()
->>>>>>> ravneet
                         .append("_id", new ObjectId())
                         .append(imageIdField, image.getImageId())
                         .append(titleField, image.getTitle())
@@ -138,16 +109,9 @@ public class ImageDB {
                         .append(priceField, image.getPrice())
                         .append(statusField, image.getStatus())
                         .append(imageLocationField, image.getImageLocation())
-<<<<<<< HEAD
                         .append(tagsField,image.getTags())
                 );
             }
-
-=======
-                        .append(tagsField, image.getTags())
-                );
-            }
->>>>>>> ravneet
             try {
                 collection.insertMany(imageList);
             } catch (MongoException me) {
@@ -156,11 +120,7 @@ public class ImageDB {
         }
     }
 
-<<<<<<< HEAD
-    public Image find(int imageId){
-=======
     public Image find(long imageId) {
->>>>>>> ravneet
         CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
         CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
 
@@ -180,13 +140,7 @@ public class ImageDB {
 
             MongoDatabase database = mongoClient.getDatabase(DB_NAME);
             MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
-
-<<<<<<< HEAD
             Document query = new Document().append(imageIdField,  image.getImageId());
-=======
-            Document query = new Document().append(imageIdField, image.getImageId());
->>>>>>> ravneet
-
             Bson updates = Updates.combine(
                     Updates.set(imageIdField, image.getImageId()),
                     Updates.set(titleField, image.getTitle()),
@@ -197,11 +151,7 @@ public class ImageDB {
                     Updates.set(priceField, image.getPrice()),
                     Updates.set(statusField, image.getStatus()),
                     Updates.set(imageLocationField, image.getImageLocation()),
-<<<<<<< HEAD
                     Updates.set(tagsField,image.getTags())
-=======
-                    Updates.set(tagsField, image.getTags())
->>>>>>> ravneet
             );
 
             UpdateOptions options = new UpdateOptions().upsert(true);
@@ -214,7 +164,6 @@ public class ImageDB {
         }
     }
 
-<<<<<<< HEAD
     public void delete(Image image) {
         try (MongoClient mongoClient = MongoClients.create(URI)) {
 
@@ -231,41 +180,19 @@ public class ImageDB {
         }
     }
 
+    public Image findByTag(String tag){
+        CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+        CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
 
-}
-=======
-        public void delete(Image image) {
-            try (MongoClient mongoClient = MongoClients.create(URI)) {
+        Image image;
 
-                MongoDatabase database = mongoClient.getDatabase(DB_NAME);
-                MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
-
-                Bson query = Filters.eq(imageIdField, image.getImageId());
-
-                try {
-                    collection.deleteOne(query);
-                } catch (MongoException me) {
-                    System.err.println("Unable to delete due to an error: " + me);
-                }
-
-            }
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+            MongoDatabase database = mongoClient.getDatabase(DB_NAME).withCodecRegistry(pojoCodecRegistry);
+            MongoCollection<Image> collection = database.getCollection(COLLECTION_NAME, Image.class);
+            image = collection.find(Filters.eq(tagsField, tag)).projection(Projections.excludeId()).first();
         }
-
-        public Image findByTag(String tag){
-            CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
-            CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
-
-            Image image;
-
-            try (MongoClient mongoClient = MongoClients.create(URI)) {
-                MongoDatabase database = mongoClient.getDatabase(DB_NAME).withCodecRegistry(pojoCodecRegistry);
-                MongoCollection<Image> collection = database.getCollection(COLLECTION_NAME, Image.class);
-                image = collection.find(Filters.eq(tagsField, tag)).projection(Projections.excludeId()).first();
-            }
-            return image;
-        }
+        return image;
+    }
 
 
 }
-
->>>>>>> ravneet
