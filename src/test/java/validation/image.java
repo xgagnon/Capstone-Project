0500f6e4c785efe156564f618858efdbdee2851e
David <david.doan01@edu.sait.ca>
2023-07-04 13:17:35 -0600

    Finished image validation testing file.

--- src/test/java/validation/image.java
+++ src/test/java/validation/image.java
@@ -10,7 +10,8 @@ public class ImageValidationTest {
 
     @Test
 public void testimageIdError() {
-    Long expectedCode = "000000000001L";
+
+        Long expectedCode = "000000000001L";
     }
 }
 
@@ -32,12 +33,30 @@ public void testSeller(){
 @Test
 public void testLikes(){
 
+    Int expectedInt = -1;
 }
 
 @Test
 public void testViews(){
-
+    Int expectedInt = -1;
 }
 
 @Test
-public void test
+public void testPrice(){
+    Double expectedDouble = -0.1;
+}
+
+@test
+public void testStatus(){
+
+}
+
+@test
+public void testImageLocation(){
+
+}
+
+@test
+public void testTags(){
+
+}
